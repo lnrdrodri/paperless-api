cdba53e0e1c87c26fc73893580ea0503c93984dd
catalan <matheus.catalan@talensesgroup.com>
2025-02-14 20:44:41 -0300

    feat: change default in is_deleted

--- db/migrate/20250127182345_change_to_false.rb
+++ db/migrate/20250127182345_change_to_false.rb
@@ -1,6 +1,6 @@
 class ChangeToFalse < ActiveRecord::Migration[7.1]
   def change
-    change_column_null :units, :is_deleted, default: false
-    change_column_null :participants, :is_deleted, default: false
+    change_column_default :units, :is_deleted, from: nil, to: false
+    change_column_default :participants, :is_deleted, from: nil, to: false
   end
 end
